46e5e64cf9c42d862e57aa0a1b0272f8c56dbee7
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-09 15:44:11 -0300

    Add components ProductList with real data from api

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,7 +3,6 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import ProductsDetail from "./components/ProductsDetail";
-import ProductListItem from "./components/ProductListItem";
 import Home from "./components/Home";
 
 export default function Routes() {
@@ -11,9 +10,8 @@ export default function Routes() {
     <Router>
       <Header />
       <Route exact path="/" component={Home} />
-      <Route exact path="/listaProducto" component={ProductList} />
-      <Route exact path="/product" component={ProductListItem} />
-      <Route exact path="/detalles" component={ProductsDetail} />
+      <Route exact path="/products" component={ProductList} />
+      <Route exact path="/products/:id" component={ProductsDetail} />
     </Router>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./ProductList.scss";
+import ProductListItem from "./ProductListItem";
 
 export default function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.mercadolibre.com/sites/MLA/search?q=zapatos&limit=4")
+      .then((res) => res.json())
+      .then((res) => setProducts(res.results));
+  });
+
   return (
-    <div>
-      
+    <div className="lista-productos">
+      {products.map((product) => (
+        <div className="style-product">
+          <ProductListItem
+            texto={product.price}
+            nombre={product.title}
+            imagen={product.thumbnail}
+          />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
