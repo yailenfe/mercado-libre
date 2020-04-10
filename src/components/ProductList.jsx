f28f1a278b5a0b8beda9bb061ab2cfca7f948aa0
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-10 00:47:24 -0300

    Add dynamic q to ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./ProductList.scss";
 import ProductListItem from "./ProductListItem";
 
-export default function ProductList() {
+export default function ProductList(props) {
   const [products, setProducts] = useState([]);
+  function useQuery() {
+    return new URLSearchParams(props.location.search);
+  }
+  const query = useQuery();
+  const q = query.get("q");
 
   useEffect(() => {
-    fetch("https://api.mercadolibre.com/sites/MLA/search?q=zapatos&limit=4")
+    fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + q + "&limit=15")
       .then((res) => res.json())
       .then((res) => setProducts(res.results));
   });
