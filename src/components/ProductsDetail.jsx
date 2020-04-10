93e4b784aafec8095ed28a8066618d28e979c71b
Yailen Fajardo Egues <yailenfajardoegues@gmail.com>
2020-04-10 00:09:01 -0300

    finish component ProductDeatail

--- src/components/ProductsDetail.jsx
+++ src/components/ProductsDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./ProductsDetail.scss";
 
-export default function ProductListItem() {
+export default function ProducDetail() {
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.mercadolibre.com/items/MLA644968197")
+      .then((res) => res.json())
+      .then((res) => {
+        const oldProduct = res;
+        fetch("https://api.mercadolibre.com/items/MLA644968197/description")
+          .then((res2) => res2.json())
+          .then((res2) => {
+            setProduct({
+              ...oldProduct,
+              description: res2.plain_text,
+            });
+          });
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>PRODUCT</h1>
+    <div className="style-contenedor">
+      <div className="style-left">
+        <img src={product.thumbnail} alt={product.title} />
+        <div>
+          <h3>Descripcion del Producto</h3>
+          <p>{product.description}</p>
+        </div>
+      </div>
+      <div className="style-right">
+        <span>
+          {product.condition} - {product.sold_quantity} vendidos
+        </span>
+        <h2>{product.title}</h2>
+        <span className="style-pecio">{product.price}</span>
+        <button>Comprar</button>
+      </div>
     </div>
-  )
+  );
 }
