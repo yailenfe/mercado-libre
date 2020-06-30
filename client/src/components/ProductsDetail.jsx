b45ec8d3f5a299276943a87e80720db5fa415eac
yailen fajardo <yailenfajardoegues@gmail.com>
2020-06-30 12:30:38 -0300

    arreglando titulos de los uatos y las imagenes

--- client/src/components/ProductsDetail.jsx
+++ client/src/components/ProductsDetail.jsx
@@ -3,42 +3,47 @@ import "./ProductsDetail.scss";
 import { useParams } from "react-router-dom";
 
 export default function ProducDetail() {
-  const [product, setProduct] = useState({});
-  const { id } = useParams();
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
 
-  useEffect(() => {
-    fetch("https://api.mercadolibre.com/items/"+ id)
-      .then((res) => res.json())
-      .then((res) => {
-        const oldProduct = res;
-        fetch("https://api.mercadolibre.com/items/"+ id + "/description")
-          .then((res2) => res2.json())
-          .then((res2) => {
-            setProduct({
-              ...oldProduct,
-              description: res2.plain_text,
+    useEffect(() => {
+        fetch("https://api.mercadolibre.com/items/" + id)
+            .then((res) => res.json())
+            .then((res) => {
+                const oldProduct = res;
+                fetch(
+                    "https://api.mercadolibre.com/items/" + id + "/description"
+                )
+                    .then((res2) => res2.json())
+                    .then((res2) => {
+                        setProduct({
+                            ...oldProduct,
+                            description: res2.plain_text,
+                        });
+                    });
             });
-          });
-      });
-  }, [id]);
+    }, [id]);
 
-  return (
-    <div className="style-contenedor">
-      <div className="style-left">
-        <img src={product.thumbnail} alt={product.title} />
-        <div>
-          <h3>Descripcion del Producto</h3>
-          <p>{product.description}</p>
+    return (
+        <div className="style-contenedor">
+            <div className="style-left">
+                <img
+                    src={product.pictures && product.pictures[0].url}
+                    alt={product.title}
+                />
+                <div>
+                    <h3>Descripcion del Producto</h3>
+                    <p>{product.description}</p>
+                </div>
+            </div>
+            <div className="style-right">
+                <span>
+                    {product.condition} - {product.sold_quantity} vendidos
+                </span>
+                <h2>{product.title}</h2>
+                <span className="style-pecio">{product.price}</span>
+                <button>Comprar</button>
+            </div>
         </div>
-      </div>
-      <div className="style-right">
-        <span>
-          {product.condition} - {product.sold_quantity} vendidos
-        </span>
-        <h2>{product.title}</h2>
-        <span className="style-pecio">{product.price}</span>
-        <button>Comprar</button>
-      </div>
-    </div>
-  );
+    );
 }
